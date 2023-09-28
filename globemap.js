d1c66fefc4f1b594026f8b05f12a3341a7541ed0
RiGael <106893713+RiGael@users.noreply.github.com>
2023-09-28 21:18:50 +0100

    Update globemap.js

--- globemap.js
+++ globemap.js
@@ -3,5 +3,5 @@ script.type = 'text/javascript';
 script.id = 'clstr_globe';
 script.src = '//clustrmaps.com/globe.js?d=dK5jSPYRyZTlDciFh6PFE_lfAK3lqo-UbY6yJNY2sVg';
 
-// Append the script to the 'visitormap' div
+// Append the script to the 'visitormap' div, This means the script will be placed inside the visitormap div, after any other content that might be inside the div.
 document.querySelector('.visitormap').appendChild(script);
