2027b89444434b104c96185c0c86d4d5bbf9e7bf
RiGael <106893713+RiGael@users.noreply.github.com>
2023-09-28 21:13:48 +0100

    Create globemap.js

---
+++ globemap.js
@@ -0,0 +1,7 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.id = 'clstr_globe';
+script.src = '//clustrmaps.com/globe.js?d=dK5jSPYRyZTlDciFh6PFE_lfAK3lqo-UbY6yJNY2sVg';
+
+// Append the script to the 'visitormap' div
+document.querySelector('.visitormap').appendChild(script);
